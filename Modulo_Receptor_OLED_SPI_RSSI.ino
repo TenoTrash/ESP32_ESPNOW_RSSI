65dfa86db5f545ee4afaa5733a934fe9459bfcbd
TenoTrash <38758218+TenoTrash@users.noreply.github.com>
2021-09-14 11:56:44 -0300

    Update Modulo_Receptor_OLED_SPI_RSSI.ino

--- Modulo_Receptor_OLED_SPI_RSSI.ino
+++ Modulo_Receptor_OLED_SPI_RSSI.ino
@@ -3,6 +3,15 @@
   Proyecto de emisor y receptor para tres entradas lógicas (para contacto seco) en emisor y tres salidas lógicas de estado en receptor
   Usando protocolo ESP NOW de Espressif
   MODULO RECEPTOR con intento de ver el RSSI del paquete recibido. Creo...
+  
+  Si esta información te resulta útil e interesante, invitame un cafecito!!!
+  
+
+  Desde fuera de Argentina en:
+  
+
+  If you found this information useful and interesting, buy me a cafecito!!!
+  
 *********/
 
 
